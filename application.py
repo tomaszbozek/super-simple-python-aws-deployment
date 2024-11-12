71577a0558677137ff4a81ef02b73f4fec7bd8c2
tomaszbozek <34103154+tomaszbozek@users.noreply.github.com>
2024-11-12 14:36:22 +0100

    Update application.py

--- application.py
+++ application.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route('/')
+@application.route('/')
 def home():
-    return "Hello, AWS Elastic Beanstalk!"
+    return "Hello, Elastic Beanstalk!"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080)
+    application.run()
